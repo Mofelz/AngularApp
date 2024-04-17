9df3514917f4a3fa3e117e7f235b2f9c5bacb5b6
Mofelz <93879842+Mofelz@users.noreply.github.com>
2024-04-17 21:30:35 +0300

    Удалены лишние файлы. Добавлены остальные проверки на все поля регистрации.

---
+++ AngularApp2.Server/Context/DazaBannixContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using AngularApp2.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularApp2.Server.Context;
+
+public partial class DazaBannixContext : DbContext
+{
+    public DazaBannixContext()
+    {
+    }
+
+    public DazaBannixContext(DbContextOptions<DazaBannixContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<EfmigrationsHistory> EfmigrationsHistories { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=127.0.0.1;port=3306;user=root;database=daza_bannix", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<EfmigrationsHistory>(entity =>
+        {
+            entity.HasKey(e => e.MigrationId).HasName("PRIMARY");
+
+            entity.ToTable("__EFMigrationsHistory");
+
+            entity.Property(e => e.MigrationId).HasMaxLength(150);
+            entity.Property(e => e.ProductVersion).HasMaxLength(32);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Login).HasMaxLength(50);
+            entity.Property(e => e.Mail).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Password).HasMaxLength(50);
+            entity.Property(e => e.Patronomic).HasMaxLength(50);
+            entity.Property(e => e.PhoneNumber).HasMaxLength(11);
+            entity.Property(e => e.Surname).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
